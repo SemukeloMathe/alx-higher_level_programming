ddd11300ceb004007cc06ca6a8271eed44b8940b
SemTheLegend <semmathe@gmail.com>
2022-08-25 06:41:24 -0700

    import function

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def add(a, b):
+    return a+b
+
+
+a = 1
+b = 2
+print(f"{a:d} + {b:d} = {add(a,b):d}")
---
+++ 0x02-python-import_modules/README.md
@@ -0,0 +1,3 @@
+0x02. Python - import & modules
+
+This folder contains solutions of tasks given in this project 
