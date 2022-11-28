9abaa17b2728666ec9c656a17de903495ce43f07
SemTheLegend <semmathe@gmail.com>
2022-11-28 10:37:58 -0800

    argv in JavaScript

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const cmdArgs = process.argv;
+
+if (cmdArgs.length <= 2) {
+  console.log('No argument');
+} else if (cmdArgs.length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
--- 0x12-javascript-warm_up/README.md
+++ 0x12-javascript-warm_up/README.md
@@ -1,1 +1,3 @@
 This repo contains alx JavaScript Warm-Up project
+
+[![js-semistandard-style](https://raw.githubusercontent.com/standard/semistandard/master/badge.svg)](https://github.com/standard/semistandard)
