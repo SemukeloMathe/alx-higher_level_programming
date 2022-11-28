8759bc87be1ede296024f39c94986e27e89a9696
SemTheLegend <semmathe@gmail.com>
2022-11-28 10:49:02 -0800

    printing first arg

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,12 @@
 #!/usr/bin/node
 const cmdArgs = process.argv;
+let count = 0;
 
-if (cmdArgs.length <= 2) {
+cmdArgs.forEach((val) => {
+  count += 1
+});
+
+if (count <= 2) {
   console.log('No argument');
 } else {
   console.log(cmdArgs[2]);
