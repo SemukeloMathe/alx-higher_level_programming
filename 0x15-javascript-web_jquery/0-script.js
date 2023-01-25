db3216cd9a0d1556a28c48fcd8b4efa325c301d2
SemTheLegend <semmathe@gmail.com>
2023-01-25 06:23:22 -0800

    0-script.js

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+/*
+ * updates the text color of the <header> element to red(#FF0000)
+ */
+
+const changeMe = document.querySelector('header');
+changeMe.style.color = '#FF0000';
---
+++ 0x15-javascript-web_jquery/README.md
@@ -0,0 +1,1 @@
+This repo contains alx projects based on alx project about JavaScript web JQuery
